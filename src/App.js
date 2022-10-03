a47cbeecbf074235563758c13e8c9858537307a9
Umer Farooq <umer.farooq@synergy.it>
2022-10-03 17:31:09 +0500

    change of css and row data

--- src/App.js
+++ src/App.js
@@ -6,9 +6,14 @@ import requests from './requests';
 function App() { 
   return (
     <div className="App">
-     <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
-     <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending}/>
-   
+      <Row title="NETFLIX ORIGINALS" fetchUrl={requests.fetchNetflixOriginals}/>
+      <Row title="TRENDING NOW" fetchUrl={requests.fetchTrending}/>
+      <Row title="TOP RATED" fetchUrl={requests.fetchTopRated}/>
+      <Row title="ACTION MOVIES" fetchUrl={requests.fetchActionMovies}/>
+      <Row title="COMEDY MOVIES" fetchUrl={requests.fetchComedyMovies}/>
+      <Row title="HORROR MOVIES" fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="ROMANCE MOVIES" fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="DOCUMENTARIES" fetchUrl={requests.fetchDocumentaries}/>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
+import './Row.css';
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
@@ -21,21 +22,15 @@ function Row({title,fetchUrl}) {
         fetchData();
 
     },[fetchUrl]);
-
-    console.table(movies);
-
+    //console.table(movies);
   return (
     <div className="row">
         <h2>{title}</h2>
-
         <div className="row__posters">
-            {/* several row__poster(s) */}
-
-            {movies.map(movie => (
-                <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
+           {movies.map(movie => (
+                <img key={movie.id} className="row__poster" src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
             ))}
         </div>
-
     </div>
   )
 }
