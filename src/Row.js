00d90ae38fa89b4373c201a61a1282139bf85f24
Umer Farooq <umer.farooq@synergy.it>
2022-10-03 16:24:25 +0500

    new changes

--- src/Row.js
+++ src/Row.js
@@ -1,15 +1,19 @@
 import React, {useState, useEffect} from 'react';
 import axios from './axios';
 
+const base_url = "https://image.tmdb.org/t/p/original/";
+
 function Row({title,fetchUrl}) {
+
     const [movies,setMovies] = useState([]);
-
+    // A snippet of code which runs based on a specific condition.
     useEffect (() => {
-
+        // if [], run once where the row loads, and don't run again.
+        
         async function fetchData(){
 
             const request =  await axios.get(fetchUrl);
-            //console.log(request.data.results);
+            //console.log(request);
             setMovies(request.data.results);
             return request;
         }
@@ -18,17 +22,17 @@ function Row({title,fetchUrl}) {
 
     },[fetchUrl]);
 
-    console.log(movies);
+    console.table(movies);
 
   return (
-    <div className='row'>
+    <div className="row">
         <h2>{title}</h2>
 
-        <div className="row_posters">
-            {/* several posters*/}
+        <div className="row__posters">
+            {/* several row__poster(s) */}
 
             {movies.map(movie => (
-                <img src="" alt=""/>
+                <img src={`${base_url}${movie.poster_path}`} alt={movie.name}/>
             ))}
         </div>
 
